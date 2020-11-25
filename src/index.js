8e6f8bddec54f215e7e245d0372ae5070ed289b8
aanand07 <bhushananand007@gmail.com>
2020-11-25 21:09:29 +0530

    setup of graphql server and prisma db.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker';
 
+// 1
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+}) // 😎
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+// 4
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
+serviceWorker.unregister();
