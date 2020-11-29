8ca30e1065e61858bf15f5e300ab00cc453b0d40
aanand07 <bhushananand007@gmail.com>
2020-11-29 08:30:33 +0530

    Implemented pagination.

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,6 +15,9 @@ class Header extends Component {
               {/* <Link to="/" className="ml1 no-underline black">
                 new
               </Link> */}
+              <Link to="/top" className="ml1 no-underline black">
+                Top
+              </Link>
               <div className="ml1">|</div>
               <Link to="/search" className="ml1 no-underline black">
                 Search
